8464a46375e81eb11f250f32ea2cf807dce55ab7
voropaevakm <60100430+voropaevakm@users.noreply.github.com>
2020-07-27 20:12:28 +0300

    Подготовка к сдаче v3: доработка формы

--- js/filter.js
+++ js/filter.js
@@ -81,7 +81,7 @@
 
   function reset() {
     filterItems.forEach(function (it) {
-      it.value === 'any';
+      it.value == 'any';
     });
     var featuresItems = featuresFieldset.querySelectorAll('input');
     featuresItems.forEach(function (feature) {
